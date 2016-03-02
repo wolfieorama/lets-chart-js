bb47a1a608c06734504dfe6e5c696d8b04e55ed5
wolfieorama <wachirawamunyi@gmail.com>
2016-03-02 16:28:50 +0300

    added chartkick rb for options and json rendering

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,8 @@ class HomesController < ApplicationController
   def show
     @visits = Visit.all
   end
+
+  def visits_by_day
+    render json: Visit.group_by_day(:visited_at, format: "B %d, %Y").count
+  end
 end
